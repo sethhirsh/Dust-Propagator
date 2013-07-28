835388c555b8d13673329388f785e4f8da91c1bd
Seth Hirsh <sethhirsh@berkeley.edu>
2013-07-27 22:55:38 -0700

    Attempting to record mean anomaly rather than true anomaly
    Mean Anomaly does not work correctly (values are too large).

--- DustPropagator/dustPropagator.cpp
+++ DustPropagator/dustPropagator.cpp
@@ -1,13 +1,34 @@
-/*    Copyright (c) 2010-2013, S. Hirsh
- *    Copyright (c) 2010-2013, Delft University of Technology
+/*    Copyright (c) 2010-2013, Delft University of Technology
  *    All rights reserved.
- *    See COPYING for license details.
+ *
+ *    Redistribution and use in source and binary forms, with or without modification, are
+ *    permitted provided that the following conditions are met:
+ *      - Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *      - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *        conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *      - Neither the name of the Delft University of Technology nor the names of its contributors
+ *        may be used to endorse or promote products derived from this software without specific
+ *        prior written permission.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *    OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *    Changelog
  *      YYMMDD    Author            Comment
  * 
+ *
  *    References
  *      
+ *
  *    Notes
  *
  */
@@ -37,7 +58,11 @@
 #include <Tudat/InputOutput/basicInputOutput.h>
 #include <Tudat/Mathematics/BasicMathematics/linearAlgebraTypes.h>
 
+#include <Tudat/Astrodynamics/BasicAstrodynamics/keplerPropagator.h>
+
 #include "DustPropagator/body.h"
+
+#include <Tudat/Mathematics/RootFinders/newtonRaphson.h>
 
 int main( )
 {
@@ -72,6 +97,13 @@ int main( )
     using tudat::unit_conversions::convertDegreesToRadians;
     using tudat::unit_conversions::convertRadiansToDegrees;
 
+    using tudat::orbital_element_conversions::convertTrueAnomalyToEccentricAnomaly;
+    using tudat::orbital_element_conversions::convertEccentricAnomalyToMeanAnomaly;
+
+    using tudat::orbital_element_conversions::propagateKeplerOrbit;
+
+    //using tudat::Mathematics::RootFinders::NewtonRaphsonPointer;
+
 
     ///////////////////////////////////////////////////////////////////////
 
@@ -81,7 +113,7 @@ int main( )
     // Values are obtained from http://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
 
     //Set output directory
-    const string outputDirectory = "";
+    const string outputDirectory = "/Users/sethmichaelhirsh/Desktop/Tudat/RawData";
 
     //Set simulation start epoch
     const double simulationStartEpoch = 0.0;
@@ -96,7 +128,8 @@ int main( )
     const double AU = 1.4959787071e11;
 
     //Eccentricity of Earth's Orbit
-    const double eccentricity  = 0.016710220;
+    //for earth eccentricity = 0.016710220
+    const double eccentricity  = 0.8;
 
 
 //Initial state of dust particle in Keplerian coordinates
@@ -117,7 +150,7 @@ int main( )
 //Set J2 zonal coefficient of the Sun
    //Source http://iau-comm4.jpl.nasa.gov/EVP.pdf "J2 = (1.7 ±0.5)×10−7"
    //unnormalized version (c or s coefficients)
-   const double J2 = 1.7e-7;
+   const double J2 = 1.9e-7;
 
 
 
@@ -168,6 +201,7 @@ double runningTime = simulationStartEpoch;
 
 DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistory;
 DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistoryInKeplerianCoordinates;
+DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistoryCOE;
 
 //Add initial state to propagation histories
 dustPropagationHistory[0.0] = dustInitialState;
@@ -185,7 +219,19 @@ dustPropagationHistoryInKeplerianCoordinates[0.0] = convertCartesianToKeplerianE
                         dustPropagationHistoryInKeplerianCoordinates[runningTime](longitudeOfAscendingNodeIndex));
 
     dustPropagationHistoryInKeplerianCoordinates[0.0](trueAnomalyIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryInKeplerianCoordinates[runningTime](trueAnomalyIndex));    
+                        dustPropagationHistoryInKeplerianCoordinates[runningTime](trueAnomalyIndex));
+
+    dustPropagationHistoryCOE[0.0](inclinationIndex) = convertRadiansToDegrees(
+                        dustPropagationHistoryInKeplerianCoordinates[runningTime](inclinationIndex));
+
+    dustPropagationHistoryCOE[0.0](argumentOfPeriapsisIndex) = convertRadiansToDegrees(
+                        dustPropagationHistoryInKeplerianCoordinates[runningTime](argumentOfPeriapsisIndex));
+
+    dustPropagationHistoryCOE[0.0](longitudeOfAscendingNodeIndex) = convertRadiansToDegrees(
+                        dustPropagationHistoryInKeplerianCoordinates[runningTime](longitudeOfAscendingNodeIndex));
+
+    dustPropagationHistoryCOE[0.0](trueAnomalyIndex) = convertRadiansToDegrees(
+                        dustPropagationHistoryInKeplerianCoordinates[runningTime](trueAnomalyIndex));
 
 
 
@@ -215,17 +261,38 @@ while (runningTime < simulationEndEpoch)
     dustPropagationHistoryInKeplerianCoordinates[runningTime](longitudeOfAscendingNodeIndex) = convertRadiansToDegrees(
                         dustPropagationHistoryInKeplerianCoordinates[runningTime](longitudeOfAscendingNodeIndex));
 
+
+    double eccentricAnomaly = convertTrueAnomalyToEccentricAnomaly(
+                            dustPropagationHistoryInKeplerianCoordinates[runningTime](trueAnomalyIndex),
+                            dustPropagationHistoryInKeplerianCoordinates[runningTime](eccentricityIndex)
+                            );
+
+    double meanAnomaly = convertEccentricAnomalyToMeanAnomaly(
+                        dustPropagationHistoryInKeplerianCoordinates[runningTime](eccentricAnomaly),
+                        dustPropagationHistoryInKeplerianCoordinates[runningTime](eccentricityIndex)
+                        );
+
     dustPropagationHistoryInKeplerianCoordinates[runningTime](trueAnomalyIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryInKeplerianCoordinates[runningTime](trueAnomalyIndex));    
+                        meanAnomaly);
+
+
+   /*dustPropagationHistoryCOE[runningTime] = propagateKeplerOrbit(
+                    dustPropagationHistoryCOE[runningTime-fixedStepSize],
+                    fixedStepSize,
+                    sunGravitationalParameter, 
+                    NewtonRaphsonPointer);  */
 
 }
+
+
+
 
 
 //////////////////////////////////////
 
 //Write DataMap to text file in Cartesian Elements
 writeDataMapToTextFile( dustPropagationHistory, 
-                        "dustPropagationHistoryWithJ2_100Years.dat",
+                        "DNICartCoordMAJ2_100Years.dat",
                         outputDirectory,
                         "",
                         numeric_limits< double >::digits10,
@@ -234,7 +301,7 @@ writeDataMapToTextFile( dustPropagationHistory,
 
 //write DataMap to text file in Keplerian Elements
 writeDataMapToTextFile( dustPropagationHistoryInKeplerianCoordinates, 
-                        "dustPropagationHistoryInKeplerianCoordinatesWithJ2_100Years.dat",
+                        "DNIKeplerCoordMAJ2_100Years.dat",
                         outputDirectory,
                         "",
                         numeric_limits< double >::digits10,
