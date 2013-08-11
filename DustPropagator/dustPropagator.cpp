0148df99567b76490f94a060dffe767093b6e62f
Seth Hirsh <sethhirsh@berkeley.edu>
2013-08-11 00:29:56 -0700

    Added Test Cases, Created new maps to output data, errors around line 302-310

--- DustPropagator/dustPropagator.cpp
+++ DustPropagator/dustPropagator.cpp
@@ -37,6 +37,7 @@
 #include <limits>
 #include <string>
 #include <utility>
+#include <sstream>
 
 #include <boost/assign/list_of.hpp>
 #include <boost/bind.hpp>
@@ -62,11 +63,11 @@
 
 #include <Tudat/Astrodynamics/BasicAstrodynamics/keplerPropagator.h>
 
-
 int main( )
 {
     using namespace std;
     using namespace dust_propagator;
+
 
     using tudat::basic_astrodynamics::AccelerationModel3dPointer;
     using tudat::basic_astrodynamics::semiMajorAxisIndex;
@@ -105,6 +106,28 @@ int main( )
 
     ///////////////////////////////////////////////////////////////////////
 
+// 6 x 5 array containing all of the test data. Each row is a different element of the initial conditions.
+//Each column is a different case. 
+
+const int numCases = 5;
+
+const double cases[6][numCases] = {
+                {360.0e3,  550.0e3, 1350.0e3, 5500.0e3, 360.0e3}, //Altitude of Perigee (m)
+                {0.0005,   0.005,   0.05,     0.1,      0.005}, //Eccentricity 
+                {10.0,     97.5,    101.0,    88.0,     10.0},  //Inclination (deg)
+                {30.0,     50.0,    80.0,     120.0,    30.0},  //Longitude of Ascending Node (deg)
+                {30.0,     30.0,    30.0,     30.0,     30.0},  //Argument of Periapsis (deg)
+                {0.0,      0.0,     0.0,      0.0,      0.0}};  //True Anomaly (deg)
+
+const int numStepSizes = 2;
+const double stepSizes[numStepSizes] = {10.0, 60.0};
+
+
+//caseNumber should span 1 to numCases
+for (int  caseNumber= 1; caseNumber <= numCases; caseNumber++) //i is the case number
+{
+    for (int stepSizeNumber = 0; stepSizeNumber < numStepSizes; stepSizeNumber++ )
+    {
 
     // Input deck
 
@@ -120,16 +143,16 @@ int main( )
     const double simulationEndEpoch = tudat::physical_constants::JULIAN_DAY*7.0;
 
     //Set numerical integration fixed step size in seconds
-    const double fixedStepSize = 60.0 ;
+    const double fixedStepSize = stepSizes[stepSizeNumber];
    
     //Astronamical Unit in meters
     //const double AU = 1.4959787071e11;
 
     //Eccentricity of Earth's Orbit around Sun = 0.016710220
-    const double eccentricity  = 0.1;
+    const double eccentricity  = cases[1][caseNumber-1];
 
     //Altitude of Perigee in meters
-    const double altitudeOfPerigee = 5500.0e3;
+    const double altitudeOfPerigee = cases[0][caseNumber-1];
 
     //Set Sun equatorial radius [in m] 6.95500e8
    //for earth 6,378.137 km
@@ -143,10 +166,10 @@ int main( )
     Vector6d dustInitialStateInKeplerianElements;
    dustInitialStateInKeplerianElements( semiMajorAxisIndex ) = semiMajorAxis;
    dustInitialStateInKeplerianElements( eccentricityIndex) = eccentricity;
-   dustInitialStateInKeplerianElements( inclinationIndex ) = convertDegreesToRadians(88.0);
-   dustInitialStateInKeplerianElements( longitudeOfAscendingNodeIndex ) = convertDegreesToRadians(120.0);
-   dustInitialStateInKeplerianElements( argumentOfPeriapsisIndex ) = convertDegreesToRadians(30.0);
-   dustInitialStateInKeplerianElements( trueAnomalyIndex ) = convertDegreesToRadians(0.0);
+   dustInitialStateInKeplerianElements( inclinationIndex ) = convertDegreesToRadians(cases[2][caseNumber-1]);
+   dustInitialStateInKeplerianElements( longitudeOfAscendingNodeIndex ) = convertDegreesToRadians(cases[3][caseNumber-1]);
+   dustInitialStateInKeplerianElements( argumentOfPeriapsisIndex ) = convertDegreesToRadians(cases[4][caseNumber-1]);
+   dustInitialStateInKeplerianElements( trueAnomalyIndex ) = convertDegreesToRadians(cases[5][caseNumber-1]);
 
 //Set Sun gravitational parameter [in m^3/s^2]
    //for Sun 1.32712440018e20
@@ -210,6 +233,7 @@ DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistoryCartCoord;
 DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistoryKeplerCoord;
 DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistoryCOEKeplerCoord;
 
+
 //Add initial state to propagation histories
 dustPropagationHistoryCartCoord[0.0] = dustInitialState;
 //dustPropagationHistoryKeplerCoord[0.0] = dustInitialStateInKeplerianElements;
@@ -234,7 +258,7 @@ while (runningTime < simulationEndEpoch)
     dustPropagationHistoryKeplerCoord[runningTime] = convertCartesianToKeplerianElements(
                   integratedState,sunGravitationalParameter);
 
-
+    //
    dustPropagationHistoryCOEKeplerCoord[runningTime] = propagateKeplerOrbit(
                     dustPropagationHistoryCOEKeplerCoord[runningTime - fixedStepSize],
                     fixedStepSize,
@@ -244,97 +268,140 @@ while (runningTime < simulationEndEpoch)
 
 
 
+//Create new data maps
 
-// Record mean anomaly in propagation history rather than true anomaly
+DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistoryDNIKeplerFinal;
+DoubleKeyTypeVectorXdValueTypeMap dustPropagationHistoryCOEKeplerFinal;
 
-runningTime = simulationStartEpoch;
-double eccentricAnomaly;
-double meanAnomaly;
+
+
+double eccentricAnomalyDNI;
+double meanAnomalyDNI;
 
 double eccentricAnomalyCOE;
 double meanAnomalyCOE;
 
-while (runningTime < simulationEndEpoch)
+enum keplerianElementsWithMeanAnomaly{
+    semiMajorAxisIndex,
+    eccentricityIndex,
+    inclinationIndex,
+    longitudeOfAscendingNodeIndex,
+    argumentOfPeriapsisIndex,
+    meanAnomalyIndex
+};
+
+
+//Convert radians to degreees for orbital elements
+//Record mean anomaly instead of true anomaly
+
+for(double currentEpoch = simulationStartEpoch; currentEpoch <= simulationEndEpoch; currentEpoch += fixedStepSize )
 {
 
-    dustPropagationHistoryKeplerCoord[runningTime] = convertCartesianToKeplerianElements(
-                  dustPropagationHistoryCartCoord[runningTime]
-                  ,sunGravitationalParameter);
-   //Convert true anomaly to eccentric anomaly
-       eccentricAnomaly = convertTrueAnomalyToEccentricAnomaly(
-                            dustPropagationHistoryKeplerCoord[runningTime](trueAnomalyIndex),
-                            dustPropagationHistoryKeplerCoord[runningTime](eccentricityIndex)
+    //Record the Direct 6 Orbital Elements in dustPropagationHistoryDNIKeplerFinal, and formatting them as specified below
+
+    //Record semiMajorAxis
+    dustPropagationHistoryDNIKeplerFinal[currentEpoch](semiMajorAxisIndex) = 
+                                    dustPropagationHistoryKeplerCoord[currentEpoch](semiMajorAxisIndex);
+
+
+    //Record eccentricity index
+    dustPropagationHistoryDNIKeplerFinal[currentEpoch](eccentricityIndex) = 
+                                    dustPropagationHistoryKeplerCoord[currentEpoch](eccentricityIndex);
+
+    //Record inclination index in degrees
+    dustPropagationHistoryDNIKeplerFinal[currentEpoch](inclinationIndex) = 
+                                    convertRadiansToDegrees(dustPropagationHistoryKeplerCoord[currentEpoch](inclinationIndex));
+
+    //Record longitude of ascending node index in degrees
+   /* dustPropagationHistoryDNIKeplerFinal[currentEpoch](longitudeOfAscendingNodeIndex) = 
+                                    convertRadiansToDegrees(dustPropagationHistoryKeplerCoord[currentEpoch](longitudeOfAscendingNodeIndex)); */
+
+    //Record argument of periapsis index in degrees
+   /* dustPropagationHistoryDNIKeplerFinal[currentEpoch](argumentOfPeriapsisIndex) = 
+                                    convertRadiansToDegrees(dustPropagationHistoryKeplerCoord[currentEpoch](argumentOfPeriapsisIndex)); */
+
+
+
+    //Convert true anomaly to eccentric anomaly
+   /* eccentricAnomalyDNI = convertTrueAnomalyToEccentricAnomaly(
+                            dustPropagationHistoryKeplerCoord[currentEpoch](trueAnomalyIndex),
+                            dustPropagationHistoryKeplerCoord[currentEpoch](eccentricityIndex)
                             );
 
     //Convert eccentric anomaly to mean anomaly
-    meanAnomaly = convertEccentricAnomalyToMeanAnomaly(
-                        eccentricAnomaly,
-                        dustPropagationHistoryKeplerCoord[runningTime](eccentricityIndex)
-                        );
+    meanAnomalyDNI = convertEccentricAnomalyToMeanAnomaly(
+                        eccentricAnomalyDNI,
+                        dustPropagationHistoryKeplerCoord[currentEpoch](eccentricityIndex)
+                        ); */
 
 
-    //Record mean anomaly in history to replace to true anomaly
-    dustPropagationHistoryKeplerCoord[runningTime](trueAnomalyIndex) = meanAnomaly;
-
-    //Convert inclinationIndex, argument of pariapsis index, longitude of ascending node index, and true anomaly Index to degrees
-    dustPropagationHistoryKeplerCoord[runningTime](inclinationIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryKeplerCoord[runningTime](inclinationIndex));
-
-    dustPropagationHistoryKeplerCoord[runningTime](argumentOfPeriapsisIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryKeplerCoord[runningTime](argumentOfPeriapsisIndex));
-
-    dustPropagationHistoryKeplerCoord[runningTime](longitudeOfAscendingNodeIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryKeplerCoord[runningTime](longitudeOfAscendingNodeIndex));
-
-    dustPropagationHistoryKeplerCoord[runningTime](trueAnomalyIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryKeplerCoord[runningTime](trueAnomalyIndex));
+    //Record mean anomaly in history rather than mean Anomaly
+    //Actually records mean anomaly not true anomaly
+   // dustPropagationHistoryDNIKeplerFinal[currentEpoch](trueAnomalyIndex) = 2.0; //meanAnomalyDNI;
 
 
 
-
-
-
+/*
 //For propagation of dust particle using COE
+
+    dustPropagationHistoryCOEKeplerFinal[currentEpoch](semiMajorAxisIndex) = 
+                    dustPropagationHistoryCOEKeplerCoord[currentEpoch](semiMajorAxisIndex);
+
+    dustPropagationHistoryCOEKeplerFinal[currentEpoch](eccentricityIndex) = 
+                    dustPropagationHistoryCOEKeplerCoord[currentEpoch](eccentricityIndex);
+
+    dustPropagationHistoryCOEKeplerFinal[currentEpoch](inclinationIndex) = 
+                    convertRadiansToDegrees(dustPropagationHistoryCOEKeplerCoord[currentEpoch](inclinationIndex));
+
+    dustPropagationHistoryCOEKeplerFinal[currentEpoch](longitudeOfAscendingNodeIndex) = 
+                    convertRadiansToDegrees(dustPropagationHistoryCOEKeplerCoord[currentEpoch](longitudeOfAscendingNodeIndex));
+
+    dustPropagationHistoryCOEKeplerFinal[currentEpoch](argumentOfPeriapsisIndex) = 
+                    convertRadiansToDegrees(dustPropagationHistoryCOEKeplerCoord[currentEpoch](argumentOfPeriapsisIndex));
+
 
     //Convert true anomaly to eccentric anomaly
     eccentricAnomalyCOE = convertTrueAnomalyToEccentricAnomaly(
-                            dustPropagationHistoryCOEKeplerCoord[runningTime](trueAnomalyIndex),
-                            dustPropagationHistoryCOEKeplerCoord[runningTime](eccentricityIndex)
+                            dustPropagationHistoryCOEKeplerCoord[currentEpoch](trueAnomalyIndex),
+                            dustPropagationHistoryCOEKeplerCoord[currentEpoch](eccentricityIndex)
                             );
 
     //Convert eccentric anomaly to mean anomaly
     meanAnomalyCOE = convertEccentricAnomalyToMeanAnomaly(
                         eccentricAnomalyCOE,
-                        dustPropagationHistoryCOEKeplerCoord[runningTime](eccentricityIndex)
+                        dustPropagationHistoryCOEKeplerCoord[currentEpoch](eccentricityIndex)
                         );
 
 
     //Record mean anomaly in history to replace to true anomaly
-    dustPropagationHistoryCOEKeplerCoord[runningTime](trueAnomalyIndex) = meanAnomalyCOE;
+    //actually stores mean anomaly not true anomaly
+    dustPropagationHistoryCOEKeplerCoord[currentEpoch](trueAnomalyIndex) = 
+                    convertRadiansToDegrees(meanAnomalyCOE); */
 
-    //Convert inclinationIndex, argument of pariapsis index, longitude of ascending node index, and true anomaly Index to degrees
-    dustPropagationHistoryCOEKeplerCoord[runningTime](inclinationIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryCOEKeplerCoord[runningTime](inclinationIndex));
-
-    dustPropagationHistoryCOEKeplerCoord[runningTime](argumentOfPeriapsisIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryCOEKeplerCoord[runningTime](argumentOfPeriapsisIndex));
-
-    dustPropagationHistoryCOEKeplerCoord[runningTime](longitudeOfAscendingNodeIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryCOEKeplerCoord[runningTime](longitudeOfAscendingNodeIndex));
-
-    dustPropagationHistoryCOEKeplerCoord[runningTime](trueAnomalyIndex) = convertRadiansToDegrees(
-                        dustPropagationHistoryCOEKeplerCoord[runningTime](trueAnomalyIndex)); 
-
-
-    runningTime = runningTime + fixedStepSize;
 }
 
 
 //////////////////////////////////////
 
+//converts case number and stepsize values as strings
+stringstream convertCaseNumber;
+stringstream convertFixedStepsize;
+convertCaseNumber << caseNumber;
+convertFixedStepsize << fixedStepSize;
+string strCaseNumber = convertCaseNumber.str();
+string strFixedStepSize = convertFixedStepsize.str();
+
+//concatenates the caseNumber and fixedStepSize 
+//to be recorded in the name of the outputted file
+//Note: the "s" is for seconds
+string definingInformation = "Case" + strCaseNumber + "_" + strFixedStepSize + "s";
+
+
+
+
 //Write DataMap to text file in Cartesian Elements
 writeDataMapToTextFile( dustPropagationHistoryCartCoord, 
-                        "dustPropagationHistoryDNICartCase4_60.dat",
+                        "dustPropagationHistoryDNICart" + definingInformation + ".dat",
                         outputDirectory,
                         "",
                         numeric_limits< double >::digits10,
@@ -343,7 +410,7 @@ writeDataMapToTextFile( dustPropagationHistoryCartCoord,
 
 //write DataMap to text file in Keplerian Elements
 writeDataMapToTextFile( dustPropagationHistoryKeplerCoord, 
-                        "dustPropagationHistoryDNICase4_60s.dat",
+                        "dustPropagationHistoryDNI" + definingInformation + ".dat",
                         outputDirectory,
                         "",
                         numeric_limits< double >::digits10,
@@ -353,14 +420,15 @@ writeDataMapToTextFile( dustPropagationHistoryKeplerCoord,
 
 //write DataMap to text file in Keplerian Elements
 writeDataMapToTextFile( dustPropagationHistoryCOEKeplerCoord, 
-                        "dustPropagationHistoryCOECase4_60s.dat",
+                        "dustPropagationHistoryCOE" + definingInformation + ".dat",
                         outputDirectory,
                         "",
                         numeric_limits< double >::digits10,
                         numeric_limits< double >::digits10,
                         ",");
+}
 
-
+}
     
     return 0;
 }
